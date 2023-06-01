69cf24cb3fd3b184eab797df3a8a14877a5dbedd
Nara <101736552+Nara-On@users.noreply.github.com>
2023-06-01 12:04:33 +0200

    Update modulePicaPared.py

--- code/modulePicaPared.py
+++ code/modulePicaPared.py
@@ -10,18 +10,19 @@ import random
 from moduleDeteccion import *
 
 def picaPared():
-    # Audio comienzo juego
+	th = 25
+	
+	# Audio comienzo juego
 
 
-
-    # Bucle principal
-    while True: #(boton no pulsado)
-    	# Audio "123 picapared"
-    	t = random.randrange(0, 5, 1)
-    	time.sleep(t)
-    	# Audio "123 ya!"
-    	mov = deteccionMov()
-    	if mov == True:
-    		# Audio "Te he visto! vuelve al inicio"
-    		
-    # Audio "Has ganado!"
+	# Bucle principal
+	while True: #(boton no pulsado)
+		# Audio "123 picapared"
+		t = random.randrange(0, 5, 1)
+		time.sleep(t)
+		# Audio "123 ya!"
+		mov = deteccionMov(th)
+		if mov == True:
+			# Audio "Te he visto! vuelve al inicio"
+			
+	# Audio "Has ganado!"
