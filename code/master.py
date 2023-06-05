4a2e7b69eb272c03e9ee752ac9124f8832ee79c0
Nara <101736552+Nara-On@users.noreply.github.com>
2023-06-05 13:17:33 +0200

    Update master.py

--- code/master.py
+++ code/master.py
@@ -8,14 +8,36 @@ from modulePicaPared import *
 
 if __name__ == '__main__':
     
+    # Preparacion seleccion juegos
+    
+    # Audio
+    pygame.mixer.init()
+	soundInicio = pygame.mixer.Sound('')
+    
+    # Boton
+	BUTTON_PIN = 15
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	
+	buttonPressed = False
+	def buttonCallback(channel):
+		buttonPressed = True
+		
+	GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=buttonCallback)
+    
+    
     while True:
         # Audio seleccion juegos
+        playing = soundInicio.play()
+        while playing.get_busy():
+            pygame.time.delay(100)
         
-        if (False): # Comprovar boton pulsado 1 vez
-            print()
+        if buttonPressed: # Comprovar boton pulsado 1 vez
+            picaPared()
         else:
             if(False): # Comprovar boton pulsado 2 veces
                 print()
             else:
                 if(False): # Comprovar boton pulsado 3 veces
-                    picaPared()
+                    print()
